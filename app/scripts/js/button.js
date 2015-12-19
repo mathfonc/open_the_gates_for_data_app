da8c00474f9d652b5f15b2b0ed6330da85930adb
mathfonc <mathiasfonck@Macbook-Pro-van-Mathias.local>
2015-12-19 16:38:15 +0100

    navigation functionality

--- app/scripts/js/button.js
+++ app/scripts/js/button.js
@@ -1,6 +1,13 @@
 (function () {
+	
 	$('.b-menu').on('click', function() {
+		//animate hamburger menu
 		$('.b-container').toggleClass('open');
 		$('.b-menu').toggleClass('open');
+		
+		//show off-canvas navigation
+		$('.navigatie').toggleClass('show-nav');
+		$('.nav-list').toggleClass('show-nav');
+		
   });
 })();
