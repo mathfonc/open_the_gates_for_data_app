33f28b5e10a00a42b42e7f7686734d969753bb90
mathfonc <mathiasfonck@Macbook-Pro-van-Mathias.local>
2015-12-21 00:47:33 +0100

    Added country list from select form via JSON

--- app/scripts/js/main.js
+++ app/scripts/js/main.js
@@ -1,10 +1,12 @@
 /*
 *	Title: upstart test
-*	Modified: 07-12-2015
-*	Version: 1.0.0
+*	Created: 07-12-2015
+*	Modified: 21-12-2015
 *	Author: Mathias Fonck
 * 	-----------------------------------------------
 *	
+*	Worldbank countries:
+*	http://api.worldbank.org/countries/all?format=jsonP&prefix=jsonp_callback&per_page=300
 *	Start-up procedures to register a business (number):
 *	http://api.worldbank.org/countries/all/indicators/IC.REG.PROC?format=jsonP&prefix=jsonp_callback&date=2015&per_page=300
 *	Time required to start a business (days):
@@ -20,38 +22,107 @@
 *
 */
 
-//anonieme IIFE; wordt uitgevoerd als document is geladen
-(function () { 
-	//aanmaken van app object met eigen functionaliteiten
+(function() {
+	
+	// Describe an App object with own functionalities
 	var App = {
 		init: function() {
-			var self = this; //closure
+			var self = this;
 			
-			this.WBSTARTUPPROCEDURESAPI = "http://api.worldbank.org/countries/all/indicators/IC.REG.PROC?format=jsonP&prefix=jsonp_callback&date=2015&per_page=300";
-			this.WBTIMETOSTARTBUSINESSAPI =  "http://api.worldbank.org/countries/all/indicators/IC.REG.DURS?format=jsonP&prefix=jsonp_callback&per_page=300&date=2015";
-			this.WBNEWREGISTEREDBUSINESSESAPI = "http://api.worldbank.org/countries/all/indicators/IC.BUS.NREG?format=jsonP&prefix=jsonp_callback&per_page=300&date=2015";
-			this.WBNEWBUSINESSESDENSITYAPI = "http://api.worldbank.org/countries/all/indicators/IC.BUS.NDNS.ZS?format=jsonP&prefix=jsonp_callback&per_page=300&date=2015";
-			this.WBINTERESTRATE = "http://api.worldbank.org/countries/all/indicators/FR.INR.RINR?format=jsonP&prefix=jsonp_callback&per_page=300&date=2015";
-			this.WBINFLATION = "http://api.worldbank.org/countries/all/indicators/NY.GDP.DEFL.KD.ZG?format=jsonP&prefix=jsonp_callback&per_page=300&date=2015";
+			// UPDATE: URL for unique callbacks (will be extended by unique values)
+			this.WBCOUNTRIESAPIURL = "http://api.worldbank.org/countries/all?format=jsonP&per_page=300&prefix=jsonp_callback";
+            this.WBSTARTUPPROCEDURESAPIURL = "http://api.worldbank.org/countries/all/indicators/IC.REG.PROC?format=jsonP&prefix=jsonp_callback&date=2015&per_page=300";
+			this.WBTIMETOSTARTBUSINESSAPIURL =  "http://api.worldbank.org/countries/all/indicators/IC.REG.DURS?format=jsonP&prefix=jsonp_callback&per_page=300&date=2015";
+			this.WBNEWREGISTEREDBUSINESSESAPIURL = "http://api.worldbank.org/countries/all/indicators/IC.BUS.NREG?format=jsonP&prefix=jsonp_callback&per_page=300&date=2015";
+			this.WBNEWBUSINESSESDENSITYAPIURL = "http://api.worldbank.org/countries/all/indicators/IC.BUS.NDNS.ZS?format=jsonP&prefix=jsonp_callback&per_page=300&date=2015";
+			this.WBINTERESTRATEAPIURL = "http://api.worldbank.org/countries/all/indicators/FR.INR.RINR?format=jsonP&prefix=jsonp_callback&per_page=300&date=2015";
+			this.WBINFLATIONAPIURL = "http://api.worldbank.org/countries/all/indicators/NY.GDP.DEFL.KD.ZG?format=jsonP&prefix=jsonp_callback&per_page=300&date=2015";
 			
-			this._dataStartup = {
-				"procedures": null,
-				"time to start a business": null,
-				"registered businesses": null,
-				"bussiness density": null,
-				"interest rate": null,
-				"inflation": null
+			this._dataCountries = null;// Variable for the list of countries
+			this._dataCountry = {
+				"procedures": null, 
+				"time to start a new business": null,// 
+				"new registered businesses": null, //
+                "new registered businesses density": null,
+                "interest rate": null,
+                "inflation": null
+			}// Variable for the details of a country
+			
+			// Handlebars Cache
+			this._hbsCache = {};// Handlebars cache for templates
+			this._hbsPartialsCache = {};// Handlebars cache for partials
+			
+			// Create a clone from the JayWalker object
+			this._jayWalker = JayWalker;
+			this._jayWalker.init();
+			this._jayWalker._countryDetailsJSONPLoaded.add(function(iso2code) {
+				self.loadDatasetsFromCountry(iso2code);// Test: load details data from country
+			});
+			
+			this.registerNavigationToggleListeners();// Register All Navigation Toggle Listeners
+			
+			this.registerWindowListeners();// Register All Navigation Toggle Listeners
+			
+			// Register listeners for list layout
+			this.registerListenersForListLayout();
+			
+			this.loadCountriesFromWorldBankAPI();// Execute method loadCountriesFromWorldBankAPI(): Load countries from the Worldbank API
+		},
+		registerNavigationToggleListeners: function() {
+			var toggles = document.querySelectorAll('.navigation-toggle');
+			
+			if(toggles != null && toggles.length > 0) {
+				var toggle = null;
+				
+				for(var i = 0; i < toggles.length; i++ ) {
+					toggle = toggles[i];
+					toggle.addEventListener('click', function(ev) {
+						ev.preventDefault();
+						
+						document.querySelector('body').classList.toggle(this.dataset.navtype);
+						
+						return false;
+					});	
+				}
 			}
-			
-			this.loadAPIFromWorldBank();
 		},
-		"loadAPIFromWolrdBank": function() {
-			var self = this; //closure
-			
+		registerWindowListeners: function() {
+			window.addEventListener('resize', function(ev) {
+				if(document.querySelector('body').classList.contains('offcanvas-open')) {
+					document.querySelector('body').classList.remove('offcanvas-open');
+				}
+				
+				if(document.querySelector('body').classList.contains('headernav-open')) {
+					document.querySelector('body').classList.remove('headernav-open');
+				}
+			});
 		},
-		loadStartupProceduresFromWorldBankAPI: function(iso2code) {
+		registerListenersForListLayout: function() {
+			
+			var self = this;
+			
+			var anchors = document.querySelectorAll('[data-listlayout]');
+			if(anchors != null && anchors.length > 0) {
+				_.each(anchors, function(anchor) {
+					anchor.addEventListener('click', function(ev) {
+						ev.preventDefault();
+						
+						var layout = this.dataset.listlayout;
+						var target = this.dataset.target;
+						
+						if(target == 'countries-list') {
+							self.updateCountriesUI('countries-' + layout, '#countries-' + layout + '-template');// Call updateCountriesUI method when successful
+						}
+						
+						return false;	
+					});	
+				});
+			}
+		},
+		loadCountriesFromWorldBankAPI: function() {
 			// Closure
-			var self = this, url = String.format(this.WBSTARTUPPROCEDURESAPI, iso2code, new Date().getTime());
+			// UPDATE
+			var self = this, url = String.format(this.WBCOUNTRIESAPIURL);
 			
 			// Load JSONP from corresponding API with certain URL
 			// JSONP Callback is defined by a function name in this case
@@ -60,15 +131,28 @@
 			Utils.getJSONPByPromise(url).then(
 				function(data) {
 					if(data != null) {
-						var startupProcedures = data[1]; // Get the forrest area from the selected country from JSON (second item from array, first item is paging)
-						var startupProceduresFiltered = _.filter(startupProcedures, function(startUpProceduresPerYear) {
-							return startUpProceduresPerYear.value != null;
-						});// First remove all years where value is null with LoDash
-						startupProceduresFiltered = _.sortBy(startupProceduresFiltered, function(startUpProceduresPerYear) {
-							return startUpProceduresPerYear.year;
-						});// Sorting on year
-						self._dataStartup.startupProcedures = startupProceduresFiltered;// Add the forrest area data to the details of a country
-						self.updateCountryDetailsUI('country-details', '#country-details-template');// Call updateCountryDetailsUI method when successful
+						var countries = data[1]; // Get the countries from JSON (second item from array, first item is paging)
+						var countriesFiltered = _.filter(countries, function(country) {
+							return !(/\d/.test(country.iso2Code));
+						});// First remove weird countries with LoDash + Assign data as value flor global variable _dataCountries within the App
+						var badISO2Codes = ['XT', 'XN', 'ZG', 'ZF', 'OE', 'XS', 'XR', 'XU', 'XQ', 'XP', 'ZQ', 'XO', 'XN', 'XM', 'XL', 'ZJ', 'XJ', 'XY', 'XE', 'EU', 'XC', 'JG', 'XD'];
+						self._dataCountries = _.filter(countriesFiltered, function(country) {
+							var validCountry = true, i = 0;
+							
+							while(validCountry && i < badISO2Codes.length) {
+								if(country.iso2Code == badISO2Codes[i]) {
+									validCountry = false;
+								} else {
+									i++;
+								}
+							}
+							
+							return validCountry;
+						});// Filter (weird codes: XT, XN, ZG, ZF, OE, XS, XR, XU, XQ, XP, ZQ, XO, XN, XM, XL, ZJ, XJ, XY, XE, EU, XC, JG)
+						self._dataCountries = _.sortBy(self._dataCountries, function(country) {
+							return country.name;
+						});// Sorting on country name
+						self.updateCountriesUI('countries-tiles', '#select-options-countries');// Call updateCountriesUI method when successful*/
 					}	
 				},
 				function(status) {
@@ -76,6 +160,15 @@
 				}
 			);
 		},
+		updateCountriesUI: function(hbsTmplName, hbsTmplId) {
+			if(!this._hbsCache[hbsTmplName]) {
+				var src = document.querySelector(hbsTmplId).innerHTML;// Get the contents from the specified hbs template
+				this._hbsCache[hbsTmplName] = Handlebars.compile(src);// Compile the source and add it to the hbs cache
+			}	
+			document.querySelector('.countries-list').innerHTML = this._hbsCache[hbsTmplName](this._dataCountries);// Write compiled content to the appropriate container
+		}
 	};
-	App.init();
+	
+	App.init();// Intialize the application
+	
 })();
